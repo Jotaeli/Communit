076b2d194eb391bc8f6234dfb82344a11cfd273f
João Lucas <100056719+Jotaeli@users.noreply.github.com>
2022-11-11 15:37:35 -0300

    scphp

--- Communit/pages/showCompanies.php
+++ Communit/pages/showCompanies.php
@@ -1,10 +1,19 @@
 <?php
-    session_start();
+    require __DIR__ . "/config.php";
+    include __DIR__ . "/functions.php";
     
-    if (!isset($_SESSION['companie']))
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+    
+    if(!isset($_SESSION['companie']))
     {
         die("Acesso negado! <a href=\"loginCompanies.php\">Conecte-se aqui!<a>");
     }
+
+    $sqlQuery = "SELECT id, companieName, img FROM users";
+    $allCompanies = $mysqli->query($sqlQuery);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -25,27 +34,26 @@
                     <img id="logo" src="../img/comunitlogo.png" alt="communitIcon">
                 </a>
             </div>
-            <a href="#">
-            <img src="../img/burguer.svg">
-            </a>
-            <a href="logout.php?logoutId=<?= $_SESSION['companie'] ?>">
-                Desconectar-se
+            <a href="logout.php">
+                <button id="logout">Desconectar-se</button>
             </a>
         </header>
         <section>
             <h1>
-                Empresa/Setores Online
+                Empresas Online
             </h1>
             <div id="cards">
+                <?php
+                    foreach($allCompanies as $info)
+                    {
+                        $name = $info["companieName"];
+                        $link = $info["img"];
 
+                        interprises($name, $link);
+                    }
+                ?>
             </div>
         </section>
-        <footer>
-                <p>
-                    Direitos reservados @Felipe e @João Lucas
-                </p>
-        </footer>
     </main>
-    <script src="./js/showCompanies.js"></script>
 </body>
 </html>
